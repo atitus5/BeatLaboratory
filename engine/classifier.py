bec4faef885c500f1cbb3bc4083353c82efcbfd9
atitus5 <drewtitus59@gmail.com>
2017-04-29 19:48:45 -0400

    Started work on classifiers

---
+++ engine/classifier.py
@@ -0,0 +1,30 @@
+#classifier.py
+
+import numpy as np
+from sklearn import svm
+
+# Used to classify events from MFCC features
+class SVMClassifier(object) :
+    def __init__(self):
+        super(SVMClassifier, self).__init__()
+
+        self.clf = svm.SVC()
+
+        # Map integer SVM classes to event labels
+        self.class2label = {
+            1: "",
+            2: "kick",
+            3: "snare",
+            4: "hihat" 
+        }
+
+    # Trains SVM using given matrix of MFCC features and labels
+    def train(self, mfccs, labels):
+        print "Training SVM on %d samples..." % len(labels)
+        self.clf.fit(mfccs, labels)
+        print "Completed SVM training"
+
+    # Classify event given vector of MFCCs for a given frame
+    def classify(self, mfccs):
+        return self.class2label[self.clf.predict([mfccs])[0]]
+
