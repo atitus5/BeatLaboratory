71a025ea5bc6590ef0a1678f3a6561d34c6e51a9
atitus5 <drewtitus59@gmail.com>
2017-05-15 10:54:17 -0400

    Added small note about pause in menu text

--- engine/blab.py
+++ engine/blab.py
@@ -47,6 +47,8 @@ Simply press anywhere outside of this box to begin.'''
 
 kPopupTitle2 = "You're ready to go!"
 kPopupMessage2 = '''You're all ready to play!
+
+Press 'p' to pause/unpause the game.
 Simply press anywhere outside of this box to begin.'''
 
 # MAIN WIDGET
